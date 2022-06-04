1b0310adcd5add68e960e481c63a84f2d0a782a6
Brayen Prayoga <brayenprayoga41@gmail.com>
2022-06-04 15:50:24 +0700

    bug view hasil admin

--- resources/views/admin/master_jawaban.blade.php
+++ resources/views/admin/master_jawaban.blade.php
@@ -94,7 +94,6 @@
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-dark" data-dismiss="modal">Close</button>
-                <button type="submit" class="btn btn-info">Simpan</button>
             </div>
         </div>
     </div>
